ca19ef65328e618d4b0fe5cca33ec25ad7ba1472
akm <akm2000@gmail.com>
2019-01-04 23:26:12 +0900

    $ goa example github.com/akm/goa_v2_post_payload_including_snake_case_fields/design

---
+++ cmd/examples/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+
+	snakecasefields "github.com/akm/goa_v2_post_payload_including_snake_case_fields"
+	user "github.com/akm/goa_v2_post_payload_including_snake_case_fields/gen/user"
+)
+
+func main() {
+	// Define command line flags, add any other flag required to configure the
+	// service.
+	var (
+		hostF     = flag.String("host", "development", "Server host (valid values: development)")
+		domainF   = flag.String("domain", "", "Host domain name (overrides host domain specified in service design)")
+		httpPortF = flag.String("http-port", "", "HTTP port (overrides host HTTP port specified in service design)")
+		secureF   = flag.Bool("secure", false, "Use secure scheme (https or grpcs)")
+		dbgF      = flag.Bool("debug", false, "Log request and response bodies")
+	)
+	flag.Parse()
+	// Setup logger and goa log adapter. Replace logger with your own using
+	// your log package of choice. The goa.design/middleware/logging/...
+	// packages define log adapters for common log packages.
+	var (
+		logger *log.Logger
+	)
+	{
+		logger = log.New(os.Stderr, "[snakecasefields] ", log.Ltime)
+	}
+	// Initialize the services.
+	var (
+		userSvc user.Service
+	)
+	{
+		userSvc = snakecasefields.NewUser(logger)
+	}
+	// Wrap the services in endpoints that can be invoked from other services
+	// potentially running in different processes.
+	var (
+		userEndpoints *user.Endpoints
+	)
+	{
+		userEndpoints = user.NewEndpoints(userSvc)
+	}
+	// Create channel used by both the signal handler and server goroutines
+	// to notify the main goroutine when to stop the server.
+	errc := make(chan error)
+
+	// Setup interrupt handler. This optional step configures the process so
+	// that SIGINT and SIGTERM signals cause the services to stop gracefully.
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		errc <- fmt.Errorf("%s", <-c)
+	}()
+
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	// Start the servers and send errors (if any) to the error channel.
+	switch *hostF {
+	case "development":
+		{
+			addr := "http://localhost:8000/"
+			u, err := url.Parse(addr)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "invalid URL %#v: %s", addr, err)
+				os.Exit(1)
+			}
+			if *secureF {
+				u.Scheme = "https"
+			}
+			if *domainF != "" {
+				u.Host = *domainF
+			}
+			if *httpPortF != "" {
+				h := strings.Split(u.Host, ":")[0]
+				u.Host = h + ":" + *httpPortF
+			} else if u.Port() == "" {
+				u.Host += ":80"
+			}
+			handleHTTPServer(ctx, u, userEndpoints, &wg, errc, logger, *dbgF)
+		}
+
+	default:
+		fmt.Fprintf(os.Stderr, "invalid host argument: %q (valid hosts: development)", *hostF)
+	}
+	// Wait for signal.
+	logger.Printf("exiting (%v)", <-errc)
+
+	// Send cancellation signal to the goroutines.
+	cancel()
+
+	wg.Wait()
+	logger.Println("exited")
+}
---
+++ user.go
@@ -0,0 +1,26 @@
+package snakecasefields
+
+import (
+	"context"
+	"log"
+
+	user "github.com/akm/goa_v2_post_payload_including_snake_case_fields/gen/user"
+)
+
+// user service example implementation.
+// The example methods log the requests and return zero values.
+type usersrvc struct {
+	logger *log.Logger
+}
+
+// NewUser returns the user service implementation.
+func NewUser(logger *log.Logger) user.Service {
+	return &usersrvc{logger}
+}
+
+// Create implements create.
+func (s *usersrvc) Create(ctx context.Context, p *user.UserPayload) (res *user.User, err error) {
+	res = &user.User{}
+	s.logger.Print("user.create")
+	return
+}
